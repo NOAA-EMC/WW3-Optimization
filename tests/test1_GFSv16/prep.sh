207606fcf3335bca7e2c91b3348a7943f24b07f3
Ali.Abdolali <37336972+aliabdolali@users.noreply.github.com>
2021-06-20 01:20:10 -0400

    Update prep.sh

--- tests/test1_GFSv16/prep.sh
+++ tests/test1_GFSv16/prep.sh
@@ -4,6 +4,8 @@ set -eux
 source default_vars.sh
 
 source edit_inputs.sh
+
+#inp prep
 edit_ww3_multi < parm/ww3_multi.inp.IN > input/ww3_multi_grdset_a.inp
 edit_ww3_fld < parm/ww3_gint.inp.IN > input/ww3_gint_grdset_a.inp
 edit_ww3_fld < parm/ww3_ounf.inp.IN > input/ww3_ounf.inp

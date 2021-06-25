78ba7d3ba656d1338b2717cae8b5a315808140df
aliabdolali <ali.abdolali@noaa.gov>
2021-06-25 21:07:59 +0000

    more

--- tests/test1_GFSv16/forcing/retreive_fix_file.sh
+++ tests/test1_GFSv16/forcing/retreive_fix_file.sh
@@ -18,6 +18,16 @@ source ../default_vars.sh
 file_rmp=`( ls ${FIXwave}rmp_src_to_dst_conserv* )`
 cp ${FIXwave}rmp_src_to_dst_conserv* ../input
   echo "$file_rmp"
+cp ${FIXwave}WHTGRIDINT.bin.glo_15mxt ../input/WHTGRIDINT.bin
+  echo "WHTGRIDINT.bin.glo_15mxt"
+cp ${FIXwave}ww3_grid.inp.glo_15mxt ../input/ww3_grid_glo_15mxt.inp
+  echo "ww3_grid_glo_15mxt.inp"
+cp ${FIXwave}ww3_grid.inp.points ../input/ww3_grid_points.inp
+  echo "ww3_grid_points.inp"
+cp ${FIXwave}ww3_grid.inp.glix_10m ../input/ww3_grid_glix_10m.inp
+  echo "ww3_grid_glix_10m.inp"
+cp ${FIXwave}ww3_grid.inp.glox_10m ../input/ww3_grid_glox_10m.inp
+  echo "ww3_grid_glox_10m.inp"
   echo "copied over"
   echo ' '                                                                 
   echo '       *****************************************************************'     

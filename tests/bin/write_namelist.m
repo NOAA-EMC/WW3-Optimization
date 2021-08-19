dba198219a707369fd017db6a77f1ccc132132f9
Ali.Abdolali <37336972+aliabdolali@users.noreply.github.com>
2021-08-18 21:52:28 -0400

    Update write_namelist.m

--- tests/bin/write_namelist.m
+++ tests/bin/write_namelist.m
@@ -4,14 +4,13 @@ function [namelist] = write_namelist(namelist,var)
 % Ali Abdolali (EMC/NCEP/NOAA ali.abdolali@noaa.gov                       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%    INPUT    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%----------------------------    INPUT    ---------------------------------
 %namelist: name of target namelist
 %var:  variables (including 22 variables in the following orders for ST4
 %physics)
-%%%%%%%%%%%%%%%%%%%    example   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%---------------------------    example   ---------------------------------
 %[namelist] = write_namelist('work/namelist',var)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%--------------------------------------------------------------------------
 %variables
 BETAMAX=var(1); TAUWSHELTER=var(2); SWELLF=var(4);  SWELLF2=var(4);
 SWELLF3=var(5); SWELLF4=var(6)    ; SWELLF5=var(7); SWELLF7=var(8);
@@ -19,7 +18,7 @@ NLPROP=var(9) ; FXFM3=var(10)     ; SDSC2=var(11) ; SDSCUM=var(12);
 SDSC5=var(13) ; SDSC6=var(14)     ; SDSBR=var(15) ; SDSBCK=var(16);
 SDSHCK=var(17); SDSCOS=var(18)    ; CICE0=var(19) ; CICEN=var(20) ;
 FLAGTR=var(21); GAMMA=var(22);
-
+%--------------------------------------------------------------------------
 %write namelist
 fileID = fopen(namelist,'w');
 fprintf(fileID,['%s %1.5f%s\n'], '&SIN4 BETAMAX = ',BETAMAX,',');
@@ -53,4 +52,3 @@ fprintf(fileID,['%s %2.3f %s\n'], 'GAMMA= ',GAMMA,'/');
 %fprintf(fileID,['%s %2.2f %s\n'], 'WDTHTH',WDTHTH,'/');
 %fprintf(fileID,['%s\n'], 'END OF NAMELISTS');
 fclose(fileID);
-
